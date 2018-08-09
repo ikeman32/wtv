b1886d5a0c838d6c04530c06a3826f4d36e71c59
ikeman32 <ikeman32@yaho.com>
2018-08-08 21:37:59 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Dependencies: python 2.7, kivy, pyperclip
+
+Files: wtv.py, wtv.kv
+
+Purpose:
+
+I made this script after FB decided to cenor a webaddress that I had posted. So in response wtv(What the Vet) was born. I used kivy for the gui because I am planning to make the script into an android app so that android users can make use of it as well. Basically it will take any string of text webaddress or simple message and obfuscate it by spelling out the entire string using the Military phonetic alphabet. 
+
+I use a simple python dictionary to achive this certain characters such as spaces, punctuation marks, etc are spelled out with dashes between the phonetic letters. For instance if there is as space between the words of a string space will be spelled out like so sierra-papa-alpha-charlie-echo. I haven't yet put all the punctuation marks or other special characters in the dictionary yet but enoungh are there to do the job.
+
+How to install and use: 
+
+Just download or clone this repository to your computer and open a terminal window in the folder you put it in. Type python wtv.py, a new window will open and you will be greeted with the baic gui for What the Vet. A text area and two buttons labeled Do It and Undo It.
+
+To obfuscate a webaddress simply paste the address string into the text area and click Do It. The script will take care of the rest, you will see the phoneticaly spelled webaddress in the text area and a message will show that it has already been copied to the clipboard. All you have to do is paste it to FB or whereever you want to paste it to. Click the Undo It button and the script will convert it back to regular text.
+
+To do:
+
+Make it look pretty. Right now the gui is very basic I plan to make it visually more appealing later on but right at the moment I am going for function over asthetics.
+
+Create standalone binaries. I want to make the script into a standalone binary for all the platforms Linux, Windows, Mac, Android, Ios, etc so that users don't have to install python. But that is proving to be harder than anticipated so far I haven't had much success in this regard yet.
+
+Add all the Ascii characters.
+
+
---
+++ wtv.py
@@ -0,0 +1,67 @@
+import kivy
+
+import pyperclip
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+phonetic={'a':'alpha','b':'beta','c':'charlie','d':'delta','e':'echo','f':'foxtrot',
+'g':'golf','h':'hotel','i':'india','j':'juliet','k':'kilo','l':'lima','m':'mike','n':'november',
+'o':'oscar','p':'papa','q':'quebec','r':'romeo','s':'sierra','t':'tango','u':'uniform',
+'v':'victor','w':'whiskey','x':'xray','y':'yankey','z':'zulu','\x2E':'delta-oscar-tango',
+'\x3A':'charlie-oscar-lima-oscar-november','\x2F':'sierra-lima-alpha-sierra-hotel',
+'A':'Alpha','B':'Beta','C':'Charlie','D':'Delta','E':'Echo','F':'Foxtrot',
+'\x20':'sierra-papa-alpha-charlie-echo','\x40':'alpha-tango',
+'G':'Golf','H':'Hotel','I':'India','J':'Juliet','K':'Kilo','L':'Lima','M':'Mike','N':'November',
+'O':'Oscar','P':'Papa','Q':'Quebec','R':'Romeo','S':'Sierra','T':'Tango','U':'Uniform',
+'V':'Victor','W':'Whiskey','X':'Xray','Y':'Yankey','Z':'Zulu'}
+
+alpha={'alpha':'a','beta':'b','charlie':'c','delta':'d','echo':'e','foxtrot':'f',
+'golf':'g','hotel':'h','india':'i','juliet':'j','kilo':'k','lima':'l','mike':'m','november':'n',
+'oscar':'o','papa':'p','quebec':'q','romeo':'r','sierra':'s','tango':'t','uniform':'u',
+'victor':'v','whiskey':'w','xray':'x','yankey':'y','zulu':'z','delta-oscar-tango':'\x2E',
+'charlie-oscar-lima-oscar-november':'\x3A','sierra-lima-alpha-sierra-hotel':'\x2F','sierra-papa-alpha-charlie-echo':'\x20',
+'alpha-tango':'\x40','Alpha':'A','beta':'B','Charlie':'C','Delta':'D','Echo':'E','Foxtrot':'F',
+'Golf':'G','Hotel':'H','India':'I','Juliet':'J','Kilo':'K','Lima':'L','Mike':'M','November':'N',
+'Oscar':'O','Papa':'P','Quebec':'Q','Romeo':'R','Sierra':'S','Tango':'T','Uniform':'U',
+'Victor':'V','Whiskey':'W','Xray':'X','Yankey':'Y','Zulu':'Z'}
+
+class wtv(App):
+
+    def build(self):
+        return FloatLayout()
+
+    def clipit(self, text, status):
+        address = str(text.text)
+        addy = ''
+        msg = "Stauts: Your output has been send to the clipboard: \n\n"
+        for index in address:
+            try:
+				addy += phonetic[index] + ' '
+            except KeyError:
+                pass
+        
+        pyperclip.copy(addy)
+        status.text = msg
+        text.text = addy
+        
+    def pasteit(self, text, status):
+	    #text.text = pyperclip.paste()
+        address = str(text.text)
+        addy = ''
+        msg = "Status: Your output has been send to the clipboard: \n\n"
+        for index in address.split():
+            try:
+				addy += alpha[index]
+            except KeyError:
+                pass
+        
+        pyperclip.copy(addy)
+        status.text = msg
+        text.text = addy
+		
+
+
+if __name__ == '__main__':
+    wtv().run()
